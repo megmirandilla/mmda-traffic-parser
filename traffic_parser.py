57a42fda36595bd6689ddfc0d4878948a8aa3eb8
Guest Admin <mamirandilla@up.edu.ph>
2018-06-13 16:18:29 +0800

    Lists all file names in given directory with absolute path

--- traffic_parser.py
+++ traffic_parser.py
@@ -1,11 +1,30 @@
 import json
 import pprint
-import os
-# with open('jhernandez-traffic-status-20180531140300.json') as j:
+import glob
+
+# import csv
+ 
+# myData = [["first_name", "second_name", "Grade"],
+#           ['Alex', 'Brian', 'A'],
+#           ['Tom', 'Smith', 'B']]
+ 
+# myFile = open('example2.csv', 'w')
+# with myFile:
+#     writer = csv.writer(myFile)
+#     writer.writerows(myData)
+     
+# print("Writing complete")
+
+# ../mmda-traffic-scrapped/out
+directory = input("Enter directory: ")
+file_names = glob.glob(directory + "/*.json")
+
+# print("\nDirection:\n[1] Northbound\n[2] Southbound")
+# direction = input("choice: ")
+
+# road = input("Enter Road Name: ")
+
+# with open(file_names[0]) as j:
 # 	data = json.load(j)
 
 # pprint.pprint(data)
-
-directory = input("Enter directory: ")
-
-file_names = os.listdir(directory)
